ee5f0abdc9cf9654d31415d2b262c492dc497c3c
AlexanderNievesC <paulalexander.nieves@gmai.com>
2023-06-04 19:46:02 -0500

    chore:add properties to main card

--- code.js
+++ code.js
@@ -1,6 +1,8 @@
-//Show Palette
-function ShowPalette(){
+//  Toggle Palette
 
+    let element=document.querySelector("#acuarela")
+    let content=document.querySelector("#main-palette")
 
-
-}
+    element.addEventListener("click",function(){
+      content.classList.toggle("palette-hidden")
+    })
